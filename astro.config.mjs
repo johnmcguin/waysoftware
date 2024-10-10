94ea33d9b5ac96208e9b493f223f6ae30bbd91f5
John McGuin <john@waysoftware.dev>
2024-10-10 11:18:14 -0600

    task: prettier write all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
 });
