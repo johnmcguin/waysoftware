77a266113f562f4678580ccfe59d4ee34618c5d9
John McGuin <john@waysoftware.dev>
2024-10-25 11:27:52 -0600

    refactor: services open menu icon
    This replaces the normal chevron-up icon with
    the mini version of it. This matches the corresponding
    chevron-down

--- src/components/nav_services.svelte
+++ src/components/nav_services.svelte
@@ -29,17 +29,14 @@
   {:else}
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
+      viewBox="0 0 20 20"
+      fill="currentColor"
       class="h-5 w-5"
     >
       <path
         fill-rule="evenodd"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="m4.5 15.75 7.5-7.5 7.5 7.5"
+        d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z"
+        clip-rule="evenodd"
       />
     </svg>
   {/if}
